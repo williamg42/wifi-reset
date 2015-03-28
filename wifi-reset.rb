e57422f6c946e4bcfa17ea073a8c0a82df0ce6fe
williamg42 <willig2@vt.edu>
2015-03-28 19:35:29 -0400

    Increased sleep timing to allow dongle to boot

--- wifi-reset.rb
+++ wifi-reset.rb
@@ -4,7 +4,7 @@ attempts = 0
 while (devices = `iwconfig 2> /dev/null`.split("\n").collect{|line| line =~ /^(\w+)/ && $1}.compact).empty? && attempts < 60  
   attempts += 1
   puts "NO wireless devices found (#{attempts})..."
-  sleep 1
+  sleep 30
 end
 
 devices.each{|device| `ifdown #{device}`}
